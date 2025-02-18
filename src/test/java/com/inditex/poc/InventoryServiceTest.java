cae4eebc9b021c7645300ae906087419d60d4769
pedromsb98 <pedromantillasa98@gmail.com>
2025-02-18 11:18:34 +0100

    cambios en paquete test y docker-compose

--- src/test/java/com/inditex/poc/InventoryServiceTest.java
+++ src/test/java/com/inditex/poc/InventoryServiceTest.java
@@ -1,4 +1,43 @@
 package com.inditex.poc;
 
+import com.inditex.poc.model.Inventory;
+import com.inditex.poc.repository.InventoryRepository;
+import com.inditex.poc.service.InventoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 public class InventoryServiceTest {
+
+    @Mock
+    private InventoryRepository inventoryRepository;
+
+    @InjectMocks
+    private InventoryService inventoryService;
+
+    private Inventory inventory;
+
+    @BeforeEach
+    public void setUp() {
+        inventory = new Inventory();
+        inventory.setId(1L);
+        inventory.setProductName("Producto 1");
+        inventory.setStockQuantity(100);
+    }
+
+    @Test
+    public void testCreateInventory() {
+        when(inventoryRepository.save(inventory)).thenReturn(inventory);
+
+        Inventory createdInventory = inventoryService.createInventory(inventory);
+
+        assertNotNull(createdInventory);
+        assertEquals("Producto 1", createdInventory.getProductName());
+
+        verify(inventoryRepository, times(1)).save(inventory);
+    }
 }
--- src/test/java/com/inditex/poc/OrderServiceTest.java
+++ src/test/java/com/inditex/poc/OrderServiceTest.java
@@ -1,4 +1,29 @@
 package com.inditex.poc;
 
+import com.inditex.poc.model.Order;
+import com.inditex.poc.repository.OrderRepository;
+import com.inditex.poc.service.OrderService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
 public class OrderServiceTest {
+    private final OrderRepository orderRepository = mock(OrderRepository.class);
+    private final OrderService orderService = new OrderService(orderRepository);
+
+    @Test
+    void testSaveOrder() {
+        Order order = new Order();
+        order.setProductName("Producto A");
+        order.setQuantity(10);
+
+        when(orderRepository.save(Mockito.any(Order.class))).thenReturn(order);
+
+        Order savedOrder = orderService.saveOrder(order);
+
+        assertNotNull(savedOrder);
+        verify(orderRepository, times(1)).save(order);
+    }
 }
+
